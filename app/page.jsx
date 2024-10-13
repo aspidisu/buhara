66115ed341d321bc51f9c2492652c13a0d975c77
aspidisu <mehmetbardakci@tutamail.com>
2024-10-13 21:05:54 +0300

    title düzenlendi

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,10 @@
 import PomodoroTimer from "./components/pomordro-timer";
 
+export const metadata = {
+  title: "Buhara Pomodoro Zamanlayıcı",
+  description: "Buhara Pomodoro Zamanlayıcı",
+};
+
 export default function Home() {
   return (
     <div className="">
